9fded1027cbc2211dbe12bc09258094e70e5fd96
Rodrigo Góes <rodriguinho-1993@hotmail.com>
2018-02-09 21:21:07 -0200

    09.02

--- index.php
+++ index.php
@@ -10,24 +10,391 @@
   <link rel="shortcut icon" type="image/jpg" href="favicon.png" />
     <title>loja Brasileiras</title>
   </head>
-  <body>
+<body>
 
-<div class="row" style="background-color: green;">
-  <div class="col-md-4">
-    <img src="icone.png" style="width: 200px; margin-left: 200px;">
-  </div>
+<div class="container-fluid">
 
-  <div class="col-md-4" style="margin-top: 20px;">
+  <div class="row" style="background-color: green;">
+
+    <div class="col-md-4">
+      <img src="icone.png" style="width: 60%; height: auto">
+    </div>
+
+  <div class="col-md-4" style="margin-top: 60px; width: 100%">
       <form class="form-inline text-center" >
-      <h1 style="color: yellow;" style="text-align: center";>Loja Brasileiras</h1>
-        <input class="form-control" type="search" placeholder="Search" aria-label="Search" style="width: 100%">
+        <input class="form-control" type="search" placeholder="O que você quer agora? :)" aria-label="Search" style="width: 100%">
       </form>
   </div>
 
 
-  <div class="col-md-4">
-    <a href="">Cadastre-se</a>
+  <div class="col-md-4" style="margin-top: 60px; text-align: center">
+    <a style="color: yellow" href="">Cadastre-se</br></a>
+    <a style="color: yellow" href="">Login</a>
   </div>
+
+</div>
+
+<div class="row">
+    <div class="col-md-3" style="background-color: #28a745"></div>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-success col-md-6">
+  <a class="navbar-brand" href="#"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+
+      <li class="nav-item active">
+            <a style="color: #fff" class="nav-link" href="#">LIVROS <span class="sr-only">(current)</span></a>
+      </li>
+
+      <li class="nav-item active">
+            <a style="color: #fff" class="nav-link" href="#">CELULARES E TELEFONES <span class="sr-only">(current)</span></a>
+      </li>
+
+      <li class="nav-item active">
+            <a style="color: #fff" class="nav-link" href="#">CONSOLES E GAMES <span class="sr-only">(current)</span></a>
+      </li>
+
+      <li class="nav-item active">
+            <a style="color: #fff" class="nav-link" href="#">ELETRODOMÉSTICOS <span class="sr-only">(current)</span></a>
+      </li>
+
+      <li class="nav-item active">
+            <a style="color: #fff" class="nav-link" href="#">INFORMÁTICA <span class="sr-only">(current)</span></a>
+      </li>
+
+      <li class="nav-item active">
+            <a style="color: #fff" class="nav-link" href="#">MÓVEIS E DECORAÇÕES <span class="sr-only">(current)</span></a>
+      </li>
+  
+    </ul>
+      </div>
+
+
+</nav>
+
+    <div class="col-md-3" style="background-color: #28a745"></div>
+
+</div>
+
+<div class="container">
+  <br>
+  <div id="carousel-example-2" class="carousel slide carousel-fade" data-ride="carousel">
+    <!--Indicators-->
+    <ol class="carousel-indicators">
+        <li data-target="#carousel-example-2" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-2" data-slide-to="1"></li>
+        <li data-target="#carousel-example-2" data-slide-to="2"></li>
+    </ol>
+    <!--/.Indicators-->
+    <!--Slides-->
+    <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active">
+            <div class="view hm-black-light">
+                <img class="d-block w-100" src="s1.jpg" alt="First slide">
+                <div class="mask"></div>
+            </div>
+            <div class="carousel-caption">
+                <h3 class="h3-responsive">Light mask</h3>
+                <p>First text</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <!--Mask color-->
+            <div class="view hm-black-strong">
+                <img class="d-block w-100" src="s2.jpg" alt="Second slide">
+                <div class="mask"></div>
+            </div>
+            <div class="carousel-caption">
+                <h3 class="h3-responsive">Strong mask</h3>
+                <p>Secondary text</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <!--Mask color-->
+            <div class="view hm-black-slight">
+                <img class="d-block w-100" src="s3.jpg" alt="Third slide">
+                <div class="mask"></div>
+            </div>
+            <div class="carousel-caption">
+                <h3 class="h3-responsive">Slight mask</h3>
+                <p>Third text</p>
+            </div>
+        </div>
+    </div>
+    <!--/.Slides-->
+    <!--Controls-->
+    <a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+    <!--/.Controls-->
+</div>
+
+</div>
+
+
+<div class="container">
+<section class="text-center pb-3">
+
+    <!--Section heading-->
+    <h1 class="font-bold h3 py-5">OS MAIS VENDIDOS</h1>
+
+    <!--Grid row-->
+    <div class="row">
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-r">
+
+            <!--Card-->
+            <div class="card card-cascade narrower" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+
+                <!--Card image-->
+                <div class="view overlay hm-white-slight">
+                    <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/1.jpg" class="img-fluid" alt="">
+                    <a>
+                        <div class="mask"></div>
+                    </a>
+                </div>
+                <!--Card image-->
+
+                <!--Card content-->
+                <div class="card-body text-center">
+                    <!--Category & Title-->
+                    <a href="" class="grey-text">
+                    </a>
+                    <h4 class="card-title">
+                        <strong>
+                            <a href="">Denim trousers</a>
+                        </strong>
+                    </h4>
+
+                    <!--Description-->
+                    <p class="card-text">Neque porro quisquam est, qui dolorem ipsum quia dolor sit..
+                    </p>
+
+                    <!--Card footer-->
+                    <div class="card-footer">
+                        <span class="left font-bold">
+                            <strong>49$</strong>
+                        </span>
+                        <span class="right">
+                            <a data-toggle="tooltip" data-placement="top" title="Add to Cart">
+                                <i class="fa fa-shopping-cart grey-text"></i>
+                            </a>
+                            <a data-toggle="tooltip" data-placement="top" title="Share">
+                                <i class="fa fa-share-alt grey-text"></i>
+                            </a>
+                            <a class="active" data-toggle="tooltip" data-placement="top" title="Added to Wishlist">
+                                <i class="fa fa-heart"></i>
+                            </a>
+                        </span>
+                    </div>
+
+                </div>
+                <!--Card content-->
+
+            </div>
+            <!--Card-->
+
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-r">
+
+            <!--Card-->
+            <div class="card card-cascade narrower" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+
+                <!--Card image-->
+                <div class="view overlay hm-white-slight">
+                    <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/2.jpg" class="img-fluid" alt="">
+                    <a>
+                        <div class="mask"></div>
+                    </a>
+                </div>
+                <!--Card image-->
+
+                <!--Card content-->
+                <div class="card-body text-center">
+                    <!--Category & Title-->
+                    <a href="" class="grey-text">
+                    </a>
+                    <h4 class="card-title">
+                        <strong>
+                            <a href="">High heels</a>
+                        </strong>
+                    </h4>
+
+                    <!--Description-->
+                    <p class="card-text">Neque porro quisquam est, qui dolorem ipsum quia dolor sit.
+                    </p>
+
+                    <!--Card footer-->
+                    <div class="card-footer">
+                        <span class="left font-bold">
+                            <strong>89$</strong>
+                        </span>
+                        <span class="right">
+                            <a data-toggle="tooltip" data-placement="top" title="Add to Cart">
+                                <i class="fa fa-shopping-cart grey-text"></i>
+                            </a>
+                            <a data-toggle="tooltip" data-placement="top" title="Share">
+                                <i class="fa fa-share-alt grey-text"></i>
+                            </a>
+                            <a class="" data-toggle="tooltip" data-placement="top" title="Add to Wishlist">
+                                <i class="fa fa-heart grey-text"></i>
+                            </a>
+                        </span>
+                    </div>
+
+                </div>
+                <!--Card content-->
+
+            </div>
+            <!--Card-->
+
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-r">
+
+            <!--Card-->
+            <div class="card card-cascade narrower" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+
+                <!--Card image-->
+                <div class="view overlay hm-white-slight">
+                    <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/4.jpg" class="img-fluid" alt="">
+                    <a>
+                        <div class="mask"></div>
+                    </a>
+                </div>
+                <!--Card image-->
+
+                <!--Card content-->
+                <div class="card-body text-center">
+                    <!--Category & Title-->
+                    <a href="" class="grey-text">
+                    </a>
+                    <h4 class="card-title">
+                        <strong>
+                            <a href="">Brown coat</a>
+                        </strong>
+                    </h4>
+
+                    <!--Description-->
+                    <p class="card-text">Neque porro quisquam est, qui dolorem ipsum quia dolor sit.
+                    </p>
+
+                    <!--Card footer-->
+                    <div class="card-footer">
+                        <span class="left font-bold">
+                            <strong>59$</strong>
+                        </span>
+                        <span class="right">
+                            <a data-toggle="tooltip" data-placement="top" title="Add to Cart">
+                                <i class="fa fa-shopping-cart grey-text"></i>
+                            </a>
+                            <a data-toggle="tooltip" data-placement="top" title="Share">
+                                <i class="fa fa-share-alt grey-text"></i>
+                            </a>
+                            <a class="" data-toggle="tooltip" data-placement="top" title="Add to Wishlist">
+                                <i class="fa fa-heart grey-text"></i>
+                            </a>
+                        </span>
+                    </div>
+
+                </div>
+                <!--Card content-->
+
+            </div>
+            <!--Card-->
+
+        </div>
+        <!--Grid column-->
+
+        <!--Fourth column-->
+        <div class="col-lg-3 col-md-6 mb-r">
+
+            <!--Card-->
+            <div class="card card-cascade narrower" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+
+                <!--Card image-->
+                <div class="view overlay hm-white-slight">
+                    <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/3.jpg" class="img-fluid" alt="">
+                    <a>
+                        <div class="mask"></div>
+                    </a>
+                </div>
+                <!--Card image-->
+
+                <!--Card content-->
+                <div class="card-body text-center">
+                    <!--Category & Title-->
+                    <a href="" class="grey-text">
+                    </a>
+                    <h4 class="card-title">
+                        <strong>
+                            <a href="">Shirt</a>
+                        </strong>
+                    </h4>
+
+                    <!--Description-->
+                    <p class="card-text">Neque porro quisquam est, qui dolorem ipsum quia dolor sit.
+                    </p>
+
+                    <!--Card footer-->
+                    <div class="card-footer">
+                        <span class="left font-bold">
+                            <strong>119$</strong>
+                        </span>
+                        <span class="right">
+                            <a data-toggle="tooltip" data-placement="top" title="Add to Cart">
+                                <i class="fa fa-shopping-cart grey-text"></i>
+                            </a>
+                            <a data-toggle="tooltip" data-placement="top" title="Share">
+                                <i class="fa fa-share-alt grey-text"></i>
+                            </a>
+                            <a class="active" data-toggle="tooltip" data-placement="top" title="Added to Wishlist">
+                                <i class="fa fa-heart"></i>
+                            </a>
+                        </span>
+                    </div>
+
+                </div>
+                <!--Card content-->
+
+            </div>
+            <!--Card-->
+
+        </div>
+        <!--Fourth column column-->
+
+    </div>
+    <!--Grid row-->
+
+</section>
+<!--Section: Products v.1-->
+</div>
+
+
+</div>
+
+
+
+
+
+
+
+
 </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
